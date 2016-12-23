b31d341184fbcb9a955b73be38e293a46c9025d7
--add <ethan@lookfamily.org>
2016-12-23 14:06:43 -0800

    cleanup

--- src/server/v1/index.js
+++ src/server/v1/index.js
@@ -5,14 +5,11 @@ let express         = require('express'),
 	_               = require('lodash'),
 	path            = require('path'),
 	fs              = require('fs'),
-	io				= require('socket.io'),
+	socket_io		= require('socket.io'),
 	denon			= require('../../devices/denon');
 
 let app = express();
 module.exports.app = app;
-
-//log stream for morgan logger
-let accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'});
 
 // serve static pages
 app.use(express.static(path.join(__dirname, '../../../public')));
@@ -28,30 +25,11 @@ server.listen(8080, () => {
 	console.log('Example app listening on ' + server.address().port);
 });
 
-let ioServer = io(server);
-ioServer.on('connection', function(socket){
+let io = socket_io(server);
+io.on('connection', function(socket){
 	console.log('User connected.');
 
-	require('./socket/denon')(ioServer, socket, denon);
-
-	// socket.on('user', function(msg) {
-	// 	socket.user = msg.username;
-	// 	console.log('User connected: ' + socket.user);
-	// });
-
-	// socket.on('room', function(msg) {
-	// 	console.log(socket.user + ' wants to join: ' + msg.room);
-	// 	if (socket.room) socket.leave(socket.room);
-	// 	socket.room = msg.room;
-	// 	socket.join(msg.room);
-	// });
-
-	// socket.on('msg', function(msg){
-	// 	console.log(socket.user + '(' + msg.room + '): ' + msg.text);
-	// 	msg = _.extend(msg, { username: socket.user });
-	// 	ioServer.sockets.in(socket.room).emit('msg', msg);
-	// 	//ioServer.emit('msg', msg);
-	// });
+	require('./sockets/denon')(io, socket, denon);
 
 	socket.on('disconnect', function(){
 		console.log('User disconnected');
